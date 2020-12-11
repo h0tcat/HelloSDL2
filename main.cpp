135e4a404fa1d6b465212baf2d207277b0def507
h0tcat <h0tnori@vivaldi.net>
2020-12-11 21:44:59 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <SDL2/SDL.h>
+
+int main(int argc, char **argv)
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* window=SDL_CreateWindow(
+        "title",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        640,
+        480,
+        SDL_WINDOW_SHOWN
+    );
+
+    SDL_Renderer* render=SDL_CreateRenderer(window,-1,0);
+    SDL_SetRenderDrawColor(render,0,255,0,255);
+    SDL_RenderClear(render);
+    SDL_RenderPresent(render);
+    SDL_Delay(3000);
+
+    return 0;
+}
